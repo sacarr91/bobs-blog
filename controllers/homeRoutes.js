3a7f3ad96c7cd2258e837983c801e643df495aad
Stephanie Carr <sacarr91@gmail.com>
2024-07-19 14:22:59 -0400

    public folder

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -40,8 +40,8 @@ router.get('/blog/:id', async (req, res) => {
 
     const blog = blogData.get({ plain: true });
 
-    res.render('project', {
-      ...project,
+    res.render('blog', {
+      ...blog,
       logged_in: req.session.logged_in
     });
   } catch (err) {
@@ -55,7 +55,7 @@ router.get('/profile', withAuth, async (req, res) => {
     // Find the logged in user based on the session ID
     const userData = await User.findByPk(req.session.user_id, {
       attributes: { exclude: ['password'] },
-      include: [{ model: Project }],
+      include: [{ model: Blog }],
     });
 
     const user = userData.get({ plain: true });
