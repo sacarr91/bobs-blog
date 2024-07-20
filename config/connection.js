097447f28c6dbceb01d24cafb27ade0a576977d9
Stephanie Carr <sacarr91@gmail.com>
2024-07-19 21:43:30 -0400

    update connection config

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,12 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-let sequelize = new Sequelize(process.env.INTERNAL_DB_URL);
+let sequelize = new Sequelize(process.env.INTERNAL_DB_URL,
+  {
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  });
 
 
 module.exports = sequelize;
