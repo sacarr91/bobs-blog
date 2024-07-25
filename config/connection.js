3ee63004c319bc82420b7b348e802285a67db0ed
Stephanie Carr <sacarr91@gmail.com>
2024-07-25 19:06:40 -0400

    change host

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD,
   {
-    host: process.env.HOST0,
+    host: 'localhost',
     dialect: 'postgres',
     ssl: {
       rejectUnauthorized: false,
