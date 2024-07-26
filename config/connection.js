9088369efa105ecee565bb2ad1c14627250d3cb5
Stephanie Carr <sacarr91@gmail.com>
2024-07-25 20:18:35 -0400

    update connection

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,7 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD,
-  {
-    host: 'localhost',
-    dialect: 'postgres',
-    ssl: {
-      rejectUnauthorized: false,
-    },
-  });
+const sequelize = new Sequelize(process.env.INTERNALURL);
 
 
 module.exports = sequelize;
